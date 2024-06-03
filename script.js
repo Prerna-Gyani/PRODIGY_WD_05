77ead8afe694c92b3e21032dfa96c6e372da6739
Prerna Gyani <142766837+Prerna-Gyani@users.noreply.github.com>
2024-06-03 14:32:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = '9EHKG8DKGTCZ2NSPCHKMW5VUN';
+const apiKey = '9EHKG8FMGTCZ2NSPCHKMW5VUN';
 
 function getWeather() {
     const location = document.getElementById('locationInput').value || 'india';
